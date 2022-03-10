fb3c4ce085b1b6fe963d360b53ac0b87bf8b1c19
l00163570 <101195099+l00163570@users.noreply.github.com>
2022-03-10 12:29:50 +0000

    Logical statements and concatenation

--- HelloWorld2.R
+++ HelloWorld2.R
@@ -12,3 +12,25 @@ my_sqrt <- sqrt(z-1)
 my_sqrt
 my_data <- c(1,2,3,4) + c(0,10)
 my_data
+
+pi:10 
+15:1
+seq(0,10, 0.5)
+
+my_seq <- seq(5,10, length = 30)
+my_seq
+rep(0, times = 40)
+
+rep(0:2, times = 10)
+
+num_vect <- c(0.5, 55, -10, 6)
+num_vect
+tf <- num_vect < 10
+tf
+
+my_char <- c("My ","name ","is ")
+my_char
+my_line <- paste(my_char, collapse = "")
+my_line 
+my_line <- paste(c(my_line, "Amitesh"), collapse="")
+my_line
